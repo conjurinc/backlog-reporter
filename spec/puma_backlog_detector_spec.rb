050ee60dc42f8181f8146794bc22d61bbfc5257b
Rafał Rzepecki <divided.mind@gmail.com>
2016-09-05 12:04:54 +0200

    Spec scaffolding

--- spec/puma_backlog_detector_spec.rb
+++ spec/puma_backlog_detector_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe PumaBacklogDetector do
-  it 'has a version number' do
-    expect(PumaBacklogDetector::VERSION).not_to be nil
+  describe "#initialize" do
+    it "removes the flag file"
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "#check" do
+    it "does nothing if there is no Puma server started"
+    it "creates the flag file if the backlog is over the threshold"
+    it "removes the flag file if the backlog is below the threshold"
+  end
+
+  describe "#check_periodically" do
+    it "periodically checks the backlog"
+  end
+
+  describe "#check_in_background" do
+    it "runs a background checking thread"
   end
 end
